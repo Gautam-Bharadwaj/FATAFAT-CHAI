fcad423be130d1c8b1179ad71a2ded08a493ffa3
JHAJI01 <kr.gautamjha03@gmail.com>
2026-02-16 03:03:49 +0530

    feat: integrate auth routes into main server application

--- server/server.js
+++ server/server.js
@@ -1,1 +1,2 @@
 const express = require('express'); const app = express(); app.listen(5000, () => console.log('Server running on port 5000'));
+// Route usage\napp.use('/api/auth', require('./routes/auth'));
